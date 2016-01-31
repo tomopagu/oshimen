c6b3e7480019ad10abe1636dd4b81b89ab4e50f1
Thomas Moore <tomo@pagu.co>
2016-01-31 15:25:24 +0000

    Settings page mostly done

--- resources/views/user/settings.blade.php
+++ resources/views/user/settings.blade.php
@@ -14,23 +14,22 @@
                     <h1>Settings for {{ $user->username }}</h1>
                     <form role="form" method="POST" action="{{ url('/register') }}">
                         {!! csrf_field() !!}
-                        
+
                         <div class="form-group{{ $errors->has('color') ? ' has-error' : '' }} row">
                             <label for="color" class="col-sm-3 form-control-label">color</label>
                             <div class="col-sm-9">
-                                <input type="text" class="form-control" name="color" value="{{ old('color') }}">
-                                @if ($errors->has('color'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('color') }}</strong>
-                                    </span>
-                                @endif
+                                <select class="form-control" name="color">
+                                    @foreach ($colors as $color)
+                                        <option {{ ($user->color === $color) ? 'selected' : '' }} value="{{ $color }}">{{ $color }}</option>
+                                    @endforeach
+                                </select>
                             </div>
                         </div>
 
                         <div class="form-group{{ $errors->has('username') ? ' has-error' : '' }} row">
                             <label for="username" class="col-sm-3 form-control-label">username</label>
                             <div class="col-sm-9">
-                                <input type="text" class="form-control" name="username" value="{{ old('username') }}">
+                                <input type="text" class="form-control" name="username" value="{{ (old('username')) ? old('username') : $user->username }}">
                                 @if ($errors->has('username'))
                                     <span class="help-block">
                                         <strong>{{ $errors->first('username') }}</strong>
@@ -38,11 +37,11 @@
                                 @endif
                             </div>
                         </div>
-                        
+
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }} row">
                             <label for="email" class="col-sm-3 form-control-label">email</label>
                             <div class="col-sm-9">
-                                <input type="email" class="form-control" name="email" value="{{ old('email') }}">
+                                <input type="email" class="form-control" name="email" value="{{ (old('email')) ? old('email') : $user->email }}">
                                 @if ($errors->has('email'))
                                     <span class="help-block">
                                         <strong>{{ $errors->first('email') }}</strong>
@@ -50,7 +49,7 @@
                                 @endif
                             </div>
                         </div>
-                        
+
                         <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }} row">
                             <label for="password" class="col-sm-3 form-control-label">password</label>
                             <div class="col-sm-9">
@@ -63,8 +62,10 @@
                             </div>
                         </div>
 
+                        <hr />
+
                         <div class="form-group{{ $errors->has('new-password') ? ' has-error' : '' }} row">
-                            <label for="new-password" class="col-sm-3 form-control-label">new-password</label>
+                            <label for="new-password" class="col-sm-3 form-control-label">new password</label>
                             <div class="col-sm-9">
                                 <input type="new-password" class="form-control" name="new-password">
                                 @if ($errors->has('new-password'))
@@ -74,9 +75,9 @@
                                 @endif
                             </div>
                         </div>
-                        
+
                         <div class="form-group{{ $errors->has('new-password_confirmation') ? ' has-error' : '' }} row">
-                            <label for="new-password_confirmation" class="col-sm-3 form-control-label">confirm new-password</label>
+                            <label for="new-password_confirmation" class="col-sm-3 form-control-label">confirm new password</label>
                             <div class="col-sm-9">
                                 <input type="new-password" class="form-control" name="new-password_confirmation">
                                 @if ($errors->has('new-password_confirmation'))
@@ -86,11 +87,11 @@
                                 @endif
                             </div>
                         </div>
-                        
+
                         <div class="form-group row">
                             <div class="col-sm-9 col-sm-offset-3">
                                 <button type="submit" class="btn btn-primary btn-block">
-                                    <i class="fa fa-btn fa-user"></i>Register
+                                    <i class="fa fa-btn fa-floppy-o"></i> Save
                                 </button>
                             </div>
                         </div>
