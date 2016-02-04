699038824c06008e939f385ce1b3134c1274de05
Thomas Moore <tsanm93@gmail.com>
2016-02-04 18:17:09 +0000

    Remove the dd($_SERVER)

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -10,8 +10,6 @@
         <link rel="stylesheet" href="{{ asset('/styles/app.css') }}">
     </head>
     <body class="user-color__{{ $userColor }}">
-        {{ dd($_SERVER) }}
-
         @yield('content')
 
         <footer class="footer">
