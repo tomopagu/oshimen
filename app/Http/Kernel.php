69acbbd53d891406b00adb23700065def272772b
Thomas Moore <tsanm93@gmail.com>
2016-02-04 18:31:44 +0000

    Add fideloper's proxy package to try force https

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -16,7 +16,7 @@ class Kernel extends HttpKernel
     protected $middleware = [
         \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
         // appending custom middleware
-        \App\Http\Middleware\HttpsProtocol::class,
+        'Fideloper\Proxy\TrustProxies',
     ];
 
     /**
