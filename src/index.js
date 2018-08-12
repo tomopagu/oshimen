a6af010bd3b1bb37a77b627efe73cf02411ebbad
Thomas Moore <tomo@uchuu.io>
2018-08-12 08:21:34 +0100

    Change order & add new oshis

--- src/index.js
+++ src/index.js
@@ -24,8 +24,11 @@ import ayaka from './Idol/idols/ayaka.jpg';
 import matsushitareona from './Idol/idols/matsushitareona.jpg';
 import risano from './Idol/idols/risano.jpg';
 import naatan from './Idol/idols/naatan.jpg';
+import rinahamu from './Idol/idols/rinahamu.jpg';
 import nanase from './Idol/idols/nanase.jpg';
 import ao from './Idol/idols/ao.jpg';
+import tsukumoneru from './Idol/idols/tsukumoneru.jpg';
+import yuka from './Idol/idols/yuka.jpg';
 
 class App extends Component {
 	render() {
@@ -34,30 +37,33 @@ class App extends Component {
 				<Box col={8} md={9} pr={2}>
 					<div className="group">
 						<h2>Kami</h2>
-						<Idol name="Furukawa Mirin" group="Dempagumi.Inc" image={ mirin } link="https://twitter.com/FurukawaMirin" color="red" />
-						<Idol name="Nanase Gumi" group="Band Ja Nai Mon" image={ gumi } link="https://twitter.com/gumi_nanase" color="aqua" />
 						<Idol name="Yamato Ao" group="Nijicon" image={ ao } link="https://twitter.com/yamato__ao" color="yellow" />
 						<Idol name="Hitochibi" group="uijin" image={ hitochibi } link="https://twitter.com/hitochibi_uijin" color="blue" />
-						<Idol name="Mahiru Kurumizawa" group="Moso Calibration" image={ mahiru } link="https://twitter.com/lunch_mahiru" color="pink" />
+						<Idol name="Furukawa Mirin" group="Dempagumi.Inc" image={ mirin } link="https://twitter.com/FurukawaMirin" color="red" />
+						<Idol name="Risano" group="Lyrical School" image={ risano } link="https://twitter.com/risano_0928" color="yellow" />
 						<div className="clearfix"></div>
 					</div>
 					<div className="group">
 						<h2>General</h2>
 						<Idol name="Togaren" group="Zenbu Kimi No Sei Da" image={ togaren } link="https://twitter.com/zenkimi_togaren" color="pink" />
-						<Idol name="Risano" group="Lyrical School" image={ risano } link="https://twitter.com/risano_0928" color="yellow" />
+						<Idol name="Rinahamu" group="CY8ER" image={ rinahamu } link="https://twitter.com/rinahamu" color="aqua" />
 						<Idol name="Nanase Hirokawa" group="Wa-Suta" image={ nanase } link="https://twitter.com/tws_nanase" color="#AAE1D8" />
-						<Idol name="Rin Kaname (Perorin)" group="Beboga! // Dempagumi.Inc" image={ perorin } link="https://twitter.com/peroperorinko01" color="yellow" />
-						<Idol name="Younapi" group="You'll Melt More" image={ younapi } link="https://twitter.com/mo_younapi" color="white" />
-						<Idol name="Kanako Momota" group="Momoiro Clover" image={ kanako } link="http://ameblo.jp/momota-sd/" color="red" />
-						<Idol name="Mirei Hoshina" group="Ebichu" image={ mirei } link="http://www.lineblog.me/tag/%E6%98%9F%E5%90%8D%E7%BE%8E%E6%80%9C?blog_name=ebichu" color="pink" />
-						<Idol name="Naa-tan" group="CY8ER" image={ naatan } link="https://twitter.com/coroteyan" color="red" />
+						<Idol name="Nanase Gumi" group="Band Ja Nai Mon" image={ gumi } link="https://twitter.com/gumi_nanase" color="aqua" />
+						<Idol name="Mahiru Kurumizawa" group="Moso Calibration" image={ mahiru } link="https://twitter.com/lunch_mahiru" color="pink" />
+						<Idol name="Tsukumo Neru" group="CYNHN" image={ tsukumoneru } link="https://twitter.com/tsukumoneru" color="#02a0ea" />
 						<Idol name="Matsushita Reona" group="ManekiKecak" image={ matsushitareona } link="https://twitter.com/matsushitareona" color="red" />
+						<Idol name="YUKA EMPiRE" group="EMPiRE" image={ yuka } link="https://twitter.com/YUKA_EMPiRE" color="black" />
 						<Idol name="Ayuni D" group="BiSH" image={ ayunid } link="https://twitter.com/AYUNiD_BiSH" color="black" />
+						<Idol name="Rin Kaname (Perorin)" group="Dempagumi.Inc" image={ perorin } link="https://twitter.com/peroperorinko01" color="yellow" />
+						<Idol name="Younapi" group="You'll Melt More" image={ younapi } link="https://twitter.com/mo_younapi" color="white" />
+						<Idol name="Mirei Hoshina" group="Ebichu" image={ mirei } link="http://www.lineblog.me/tag/%E6%98%9F%E5%90%8D%E7%BE%8E%E6%80%9C?blog_name=ebichu" color="pink" />
+						<Idol name="Kanako Momota" group="Momoiro Clover" image={ kanako } link="http://ameblo.jp/momota-sd/" color="red" />
 						<Idol name="Takashima Kaede" group="PassCode" image={ kaede } link="https://twitter.com/PassCode_kaede" color="yellow" />
 						<div className="clearfix"></div>
 					</div>
 					<div className="group">
 						<h2>Ex</h2>
+						<Idol name="Naa-tan" group="CY8ER" image={ naatan } link="https://twitter.com/coroteyan" color="red" />
 						<Idol name="Ayaka" group="Lyrical School" image={ ayaka } link="https://twitter.com/ayaka831" color="blue" />
 						<Idol name="Mai Imai" group="Musubizm" image={ imai } link="https://twitter.com/musubizm_mai" color="white" />
 						<div className="clearfix"></div>
